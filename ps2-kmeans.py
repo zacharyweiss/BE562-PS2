9f8bdde479afbd9363f1f59a964db195f834112b
zacharyweiss <zachary.weiss.acct@gmail.com>
2019-09-30 13:04:38 -0400

    Reworked plotting, fuzzy working, non-fuzzy integrated into same functions as fuzzy (in form of a special case of fuzzy)

--- ps2-kmeans.py
+++ ps2-kmeans.py
@@ -1,8 +1,8 @@
 import csv
-import random
 import sys
 import argparse
 import matplotlib.pyplot as plt
+import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument('fname', type=str)
@@ -10,60 +10,150 @@ parser.add_argument('k_clusters', metavar='k', type=int)
 parser.add_argument('-fuzzy', action='store_true')
 args = parser.parse_args()
 
+# variables
+threshold = 100  # max number of iterations
+m = 2  # degree of "fuzziness"; higher -> fuzzier, default of 2
+
+# filename start int
+plot_fn = 0
+
+
 # find square distances
 def d_square(pt1, pt2, dim):
     d = 0
     for i in range(dim):
         d += (float(pt1[i]) - float(pt2[i])) ** 2
-    return d
+
+    if d == 0:
+        return d + 0.0000000000001  # correct for if points are at same location
+    else:
+        return d
+# def assign_to_c(pt, dim, c):
+#     # initializing d_min and ci before the loop with i=0 case, start on i=1 in loop
+#     # min distance squared
+#     d_min = d_square(c[0], pt, dim)
+#     # cluster index of d_min
+#     ci = c[0][dim]
+#
+#     # start loop at cluster index i=1
+#     for i in c[1:]:
+#         di = d_square(i, pt, dim)
+#         if di < d_min:  # if a new minimum distance is found
+#             d_min = di  # replace with lowest distance
+#             ci = i[dim]  # store cluster index of current cluster center
+#
+#     pt[dim] = ci
+#     return pt
 
 
-def assign_to_c(pt, dim, c):
-    # initializing d_min and ci before the loop with i=0 case, start on i=1 in loop
-    # min distance squared
-    d_min = d_square(c[0], pt, dim)
-    # cluster index of d_min
-    ci = c[0][dim]
+def assign_c_prob(pt, dim, c):
+    # The general equation for wk(x) is wk = 1 / sigma_j( (d(ck,x) / d(cj,x)) ** (2/m-1) )
 
-    # start loop at cluster index i=1
-    for i in c[1:]:
-        di = d_square(i, pt, dim)
-        if di < d_min:  # if a new minimum distance is found
-            d_min = di  # replace with lowest distance
-            ci = i[dim]  # store cluster index of current cluster center
+    # create array of square distances to each center from point
+    d = []
+    for ck in c:
+        d.append(d_square(ck, pt, dim))
 
-    pt[dim] = ci
+    # prepare to store wk "partition matrix" / probability of belonging to a center
+    wk = []
+    for i in range(len(c)):
+        d_ratio = []
+        d_k = d[i]  # current center being looked at
+
+        # create array of ratios to be summed over
+        for d_j in d:
+            # print(d_k)
+            # print(d_j)
+            # print(d_k/d_j)
+            d_ratio.append(
+                (d_k / d_j) ** (2/(m-1))
+            )
+
+        # calculate and append final wk for the current center
+        wk.append(1 / sum(d_ratio))
+
+    if not args.fuzzy:
+        ind = np.where(wk == np.amax(wk))[0][0]
+        wk = [0] * len(wk)  # replace all values with zero
+        wk[ind] = 1  # insert a one at the index of the closest (max)
+
+    # place new weighted values to each center in array in last index of pt
+    pt[dim] = wk
     return pt
 
 
-def recenter(c, data, dim):
+def recenter_prob(data, dim):
     re_c = []
-    for i in range(len(c)):
-        re_c.append([0, 0, -1])  # placeholders to be written over; no cluster exists of index -1
-
-    for i, val in enumerate(c):
-
-        # init runsums for value and count of x
-        xi = []  # sum of all x with label k (for each dim)
+    for i in range(args.k_clusters):
+        re_c.append([])
         for j in range(dim):
-            xi.append(0)
-        xk = 0  # count of xi with label k
+            re_c[i].append(0)
+        re_c[i].append(-1)
 
-        for j in range(len(data)):
-            # if a point's assigned cluster is the cluster being recentered
-            if data[j][dim] == val[dim]:
-                # increment count of points in cluster
-                xk += 1
-                # for each dim add the coordinate to the runsum
-                for k in range(dim):
-                    xi[k] += float(data[j][k])
+    if args.fuzzy:
+        for i, c_i in enumerate(re_c):
+            for j in range(dim):
+                # runsums across all points for a given dimension
+                numerator = 0
+                denominator = 0
+                for pt in data:
+                    numerator += (pt[dim][i] ** m) * pt[j]
+                    denominator += (pt[dim][i] ** m)
+                c_i[j] = numerator / denominator
+            c_i[dim] = i
+        return re_c
 
-        # assign new centroid position to c along each dim
-        for j in range(dim):
-            re_c[i][j] = xi[j] / xk
-        re_c[i][dim] = val[dim]
+    elif not args.fuzzy:
+        for i, val in enumerate(re_c):
+            # init runsums for value and count of x
+            xi = []  # sum of all x with label k (for each dim)
+            for j in range(dim):
+                xi.append(0)
+            xk = 0  # count of xi with label k
+            for j in range(len(data)):
+                # if a point's assigned cluster is the cluster being recentered
+                if np.where(data[j][dim] == np.amax(data[j][dim]))[0][0] == i:
+                    # increment count of points in cluster
+                    xk += 1
+                    # for each dim add the coordinate to the runsum
+                    for k in range(dim):
+                        xi[k] += data[j][k]
+            # assign new centroid position to c along each dim
+            for j in range(dim):
+                re_c[i][j] = xi[j] / xk
+            re_c[i][dim] = val[dim]
+        return re_c
 
-    return re_c
+#
+# def recenter(c, data, dim):
+#     re_c = []
+#     # I know this looping is funky, only like this as former version seemed to somehow be overwriting original c list
+#     for i in range(len(c)):
+#         re_c.append([0, 0, -1])  # placeholders to be written over; no cluster exists of index -1
+#
+#     for i, val in enumerate(c):
+#
+#         # init runsums for value and count of x
+#         xi = []  # sum of all x with label k (for each dim)
+#         for j in range(dim):
+#             xi.append(0)
+#         xk = 0  # count of xi with label k
+#
+#         for j in range(len(data)):
+#             # if a point's assigned cluster is the cluster being recentered
+#             if data[j][dim] == val[dim]:
+#                 # increment count of points in cluster
+#                 xk += 1
+#                 # for each dim add the coordinate to the runsum
+#                 for k in range(dim):
+#                     xi[k] += float(data[j][k])
+#
+#         # assign new centroid position to c along each dim
+#         for j in range(dim):
+#             re_c[i][j] = xi[j] / xk
+#         re_c[i][dim] = val[dim]
+#
+#     return re_c
 
 
 # open tsv, parse into list, close file
@@ -74,46 +164,87 @@ def read_tsv():
         for row in reader:
             data.append(row)
         f.close()
+    data = np.array(data).astype(float).tolist()
     return data
 
 
 # initialize k centers of dim dimensions, with cluster index as last list list item
-def init_c(dim):
+def init_c(data, dim):
+    # centers = [np.array(data[i])-0.05 for i in np.random.randint(0,len(data),k)]
     centers = []
-    for i in range(args.k_clusters):
-        centers.append([])
-        for j in range(dim):
-            centers[i].append(i + random.randint(0, 10) / 10 * (i + 1))  # rethink random location alg?
-        centers[i].append(i)
+    c_index = 0
+    for i in np.random.randint(0, len(data), args.k_clusters):
+        centers.append(data[i][:2])
+        centers[c_index].append(c_index)
+        c_index += 1
+        # for j in range(dim):
+        #     centers[i].append(i + np.random.randint(0, 10) / 10 * (i + 1))  # rethink random location alg?
+        # centers[i].append(i)
     return centers
 
 
 def plot(c, data, dim):
-    fig = plt.figure()
-    ax1 = fig.add_subplot(111)
+    global plot_fn
+    col = plt.cm.tab20c(
+        np.linspace(0, 1, args.k_clusters)
+    )
+    plt.figure()
+    data = np.array(data)
+    for i in range(args.k_clusters):
+        a = []
+        for row in data:
+            a.append(
+                np.where(row[dim] == np.amax(row[dim]))[0][0]
+            )
+        ind = [row == i for row in a]
+        plt.scatter(data[ind, 0],
+                    data[ind, 1],
+                    c=col[i],
+                    s=3)
+        plt.scatter(c[i][0],
+                    c[i][1],
+                    s=10,
+                    c=col[i],
+                    linewidths=1,
+                    edgecolors="000")
 
-    col = []
-    for i in range(len(data)):
-        if [float(row[dim]) for row in data][i] == 0:
-            col.append('k')
-        elif [float(row[dim]) for row in data][i] == 1:
-            col.append('r')
-        else:
-            col.append('b')
-
-    ax1.scatter([float(row[0]) for row in data], [float(row[1]) for row in data], c=col, s=5)
-
-    col2 = []
-    for i in range(len(c)):
-        if [float(row[dim]) for row in c][i] == 0:
-            col2.append('k')
-        elif [float(row[dim]) for row in c][i] == 1:
-            col2.append('r')
-        else:
-            col2.append('b')
-
-    ax1.scatter([float(row[0]) for row in c], [float(row[1]) for row in c], c=col2, s=15, marker="s")
+    plt.savefig("figure"+str(plot_fn)+".png")
     plt.show()
+    plot_fn += 1
+    # fig = plt.figure()
+    # ax1 = fig.add_subplot(111)
+    #
+    # col = []
+    # if not args.fuzzy:
+    #     for i in range(len(data)):
+    #         if [float(row[dim]) for row in data][i] == 0:
+    #             col.append('k')
+    #         elif [float(row[dim]) for row in data][i] == 1:
+    #             col.append('r')
+    #         else:
+    #             col.append('b')
+    # else:
+    #     for i in range(len(data)):
+    #         if [row[dim].index(max(row[dim])) for row in data][i] == 0:
+    #             col.append('k')
+    #         elif [row[dim].index(max(row[dim])) for row in data][i] == 1:
+    #             col.append('r')
+    #         else:
+    #             col.append('b')
+    #
+    # ax1.scatter([float(row[0]) for row in data], [float(row[1]) for row in data], c=col, s=5)
+    #
+    # col2 = []
+    # for i in range(len(c)):
+    #     if [float(row[dim]) for row in c][i] == 0:
+    #         col2.append('k')
+    #     elif [float(row[dim]) for row in c][i] == 1:
+    #         col2.append('r')
+    #     else:
+    #         col2.append('b')
+    #
+    # ax1.scatter([float(row[0]) for row in c], [float(row[1]) for row in c], c=col2, s=15, marker="s")
+    # plt.show()
 
 
 def main():
@@ -129,39 +260,60 @@ def main():
     # k = int(args.k_clusters)
 
     # initialize centers at randomized locations
-    centers = init_c(dim)
+    centers = init_c(data, dim)
 
-    # normal / fuzzy k-means switching
-    if not args.fuzzy:
-        # classify points to nearest centers
-        for i in range(len(data)):
-            data[i] = assign_to_c(data[i], dim, centers)
-
-        # move each center to centroid of newly labeled points
-        re_c = recenter(centers, data, dim)
-        iterations = 0
-
-        while centers != re_c and iterations < 20:
-            iterations += 1
-            # sys.stdout.write("Iteration #" + str(iterations) + "\n")
-
-            centers = re_c
-
-            for i in range(len(data)):
-                data[i] = assign_to_c(data[i], dim, centers)
-
-            re_c = recenter(centers, data, dim)
+    # # normal / fuzzy k-means switching
+    # if not args.fuzzy:
+    #     # classify points to nearest centers
+    #     for i in range(len(data)):
+    #         data[i] = assign_to_c(data[i], dim, centers)
+    #
+    #     # move each center to centroid of newly labeled points
+    #     re_c = recenter(centers, data, dim)
+    #     iterations = 1
+    #
+    #     while centers != re_c and iterations < 20:
+    #         iterations += 1
+    #         # sys.stdout.write("Iteration #" + str(iterations) + "\n")
+    #
+    #         centers = re_c
+    #
+    #         for i in range(len(data)):
+    #             data[i] = assign_to_c(data[i], dim, centers)
+    #
+    #         re_c = recenter(centers, data, dim)
     # elif args.fuzzy:
-    else:
-        sys.stderr.write("Fuzzy arg err")
-        sys.exit(1)
+    #     sys.stdout.write("Fuzzy true\n")
+
+    for i in range(len(data)):
+        data[i] = assign_c_prob(data[i], dim, centers)
+
+    re_c = recenter_prob(data, dim)
+    iterations = 1
+
+    while centers != re_c and iterations < threshold:
+        iterations += 1
+        # sys.stdout.write("Iteration #" + str(iterations) + "\n")
+
+        centers = re_c
+
+        for i in range(len(data)):
+            data[i] = assign_c_prob(data[i], dim, centers)
+
+        re_c = recenter_prob(data, dim)
+    #
+    # else:
+    #     sys.stderr.write("Fuzzy arg err\n")
+    #     sys.exit(1)
 
     for c in centers:
         sys.stdout.write("Cluster #" + str(c[dim]) + ": " + str(c[0:dim]) + "\n")
     for d in data:
         sys.stdout.write(str(d) + "\n")
 
-    sys.stdout.write("Fuzzy: "+ str(args.fuzzy) + "\n")
+    sys.stdout.write("Fuzzy: " + str(args.fuzzy) + "\n")
+    print(iterations)
+
     plot(centers, data, dim)
 
 
